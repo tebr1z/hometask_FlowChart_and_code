56174ed421e33331f3eea62f899bc5fa1523acee
tebr1z <tabrizyh@code.edu.az>
2024-02-13 19:56:05 +0400

    feat: add ortalama 10 100

---
+++ ortalama/ortalama/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+ 
+        
+
+        int sum = 0;
+
+Console.Write("Daxil et ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+        if (num1 >= 10 && num1 <= 100)
+        {
+            sum += num1;
+        }
+else
+{
+    Console.WriteLine("10ve 100 arsina bir sayi yazin"); return;
+}
+Console.Write("Daxil et ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 >= 10 && num2 <= 100)
+        {
+            sum += num2;
+        }
+else
+{
+    Console.WriteLine("10ve 100 arsina bir sayi yazin"); return;
+}
+
+Console.Write("Daxil et ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num3 >= 10 && num3 <= 100)
+        {
+            sum += num3;
+        }
+else
+{
+    Console.WriteLine("10ve 100 arsina bir sayi yazin"); return;
+}
+Console.WriteLine("10 ile 100 arasında olan sayıların toplamı: " + sum);
+ 
